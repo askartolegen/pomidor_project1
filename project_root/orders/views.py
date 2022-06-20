c4cb22539bf2d8e43640b054244ebb749f138cb9
askartolegen <askartolegen13@gmail.com>
2022-06-20 13:57:36 +0600

    Initial commit

---
+++ project_root/orders/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from .models import SalesOrder
+
+
+# Create your views here.
+from .serializers import OrderSerializer
+
+
+def orders_page(request):
+    return render(request, 'index.html', {'orders': SalesOrder.objects.all()})
+
+
+class OrderView(ModelViewSet):
+    queryset = SalesOrder.objects.all()
+    serializer_class = OrderSerializer
+
+
+def orders_app(request):
+    return render(request, 'main_app.html')
